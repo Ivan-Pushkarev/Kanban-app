aab7c0c94678f0c29df6df3f5a631cc3818797af
Ivan-Pushkarev <ivan.n.pushkarev@gmail.com>
2021-03-17 15:29:23 -0400

    final version

--- src/App.js
+++ src/App.js
@@ -3,35 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import ChangeStatus from "./ChangeStatus";
 
-const currentStatus = [
-  {
-    statusId: 0,
-    statusName: 'Pending',
-  },
-  {
-    statusId: 1,
-    statusName: 'Backlogs',
-  },
-  {
-    statusId: 2,
-    statusName: 'Todo',
-  },
-  {
-    statusId: 3,
-    statusName: 'Progress',
-  },
-  {
-    statusId: 4,
-    statusName: 'Review',
-  },
-  {
-    statusId: 5,
-    statusName: 'Done',
-  },
-
- ];
-
-
+const currentStatus = ['Backlogs','Todo','Progress','Review','Done'];
 const tasks = [
   {
     id: 1,
@@ -64,13 +36,6 @@ const tasks = [
 function App() {
 
   const [list, setList] = useState(tasks);
-  const tasksList = list.filter(el => el.status === 0);
-  const backlogList = list.filter(el => el.status === 1);
-  const todoList = list.filter(el => el.status === 2);
-  const progressList = list.filter(el => el.status === 3);
-  const reviewList = list.filter(el => el.status === 4);
-  const doneList = list.filter(el => el.status === 5);
-  
   const [inputValue, setInputValue] = useState('Add task');
   const onChange = e => {
    
@@ -87,71 +52,49 @@ function App() {
   };
   
   const leftButtonPushed = (id) => {
-    console.log(id);
     const newList = list.map(el => el.id === id ? ({...el, status: (el.status - 1)}) : el);
     setList(newList );
-   
-  
   }
   const rightButtonPushed = (id) => {
     const newList = list.map(el => el.id === id ? ({...el, status: (el.status + 1)}) : el);
     setList(newList );
-  
-  
   }
-  
-  
-  
   
   return <div>
     <h2><center className ="p-3 mb-2 bg-secondary text-white"> Kanban App</center></h2>
-    <div className="container-fluid">
+    <center>
+      <input type="text" onChange={onChange} value={inputValue} />
+      <button type="button" className="btn btn-outline-secondary" onClick={addToList}>Add</button>
+    </center>
+    <div className="container">
       <div className="row justify-content-around">
-        <div className="col-auto" class="col-sm-3 bg-light p-3 border">
-         <h4> My Tasks </h4>
-          <input type="text" onChange={onChange} value={inputValue} />
-          <button type="button" className="btn btn-secondary" onClick={addToList}>Add to list</button>
-          <div>
-            <ol>
-              {  tasksList.map(el =>
-              <li key={el.id} ><button type="button" className="btn btn-outline-secondary" onClick={() => rightButtonPushed(el.id)}> {el.name}</button></li>)}
-            </ol>
-          </div>
-        </div>
+      
         <div className="col">
           <h3> todo ⇒ </h3>
           <hr/>
           <div>
-            {
-            todoList.map(el => <div key={el.id}>
-            <ChangeStatus id={el.id} status={el.status} name={el.name} currentStatus={currentStatus}
-                          leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
-            </div>)
-            }
+            
+              <ChangeStatus list={list} status={1} currentStatus={currentStatus}
+                            leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
+            
           </div>
         </div>
         <div className="col">
           <h3> progress ⇒ </h3>
           <hr/>
             <div>
-              {
-                progressList.map(el => <div key={el.id}>
-                  <ChangeStatus id={el.id} status={el.status} name={el.name} currentStatus={currentStatus}
-                                leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
-                </div>)
-              }
+              <ChangeStatus list={list} status={2} currentStatus={currentStatus}
+                            leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
+
             </div>
         </div>
         <div className="col">
           <h3> review ⇒ </h3>
           <hr/>
           <div>
-            {
-              reviewList.map(el => <div key={el.id}>
-                <ChangeStatus id={el.id} status={el.status} name={el.name} currentStatus={currentStatus}
-                              leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
-              </div>)
-            }
+            <ChangeStatus list={list} status={3} currentStatus={currentStatus}
+                          leftButtonPushed={leftButtonPushed}  rightButtonPushed={rightButtonPushed}/>
+
           </div>
         </div>
         <div className="col">
@@ -159,16 +102,20 @@ function App() {
           <hr/>
           <div>
             {
-              doneList.map(el => <div key={el.id}>{el.name}</div>)
+              list.filter(el => el.status === 4)
+                  .map(el => <div key={el.id}>{el.name}</div>)
             }
           </div>
         </div>
       </div>
       </div>
     <center><h3>backlogs: </h3>
-      {
-        backlogList.map(el => <div><button type="button" class="btn btn-outline-secondary"  key={el.id} onClick={() => rightButtonPushed(el.id)} >{el.name}</button></div>)
-      }
+      <div>
+        {
+          list.filter(el => el.status === 0)
+              .map(el => <button type="button" className="btn btn-outline-secondary" key={el.id} onClick={()=> rightButtonPushed(el.id)}>{el.name}</button>)
+        }
+      </div>
     </center>
       </div>
   
--- src/ChangeStatus.js
+++ src/ChangeStatus.js
@@ -2,19 +2,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function Message(props) {
-    let leftButton = props.currentStatus[props.status - 1].statusName;
-    let rightButton =props.currentStatus[props.status +1].statusName;
-    
-   
-    
+    let leftButton = props.currentStatus[props.status - 1];
+    let rightButton =props.currentStatus[props.status +1];
+        
         return (
-            <div
-                className="p-3 mb-2 bg-light text-dark">
-                <h5  className="p-3 mb-2 bg-secondary text-white"> {props.name}:</h5>
-                <center>
-                <button type="button" className="btn btn-outline-secondary" onClick={() => props.leftButtonPushed(props.id) } > ⇦{leftButton}</button>
-                <button type="button" className="btn btn-outline-secondary" onClick={() => props.rightButtonPushed(props.id)} >{rightButton}⇨</button>
+            <div>
+                             
+                <center className="p-3 mb-2 bg-light text-dark">
+                    {
+                        props.list
+                            .filter(el=> el.status === props.status)
+                            .map(el => <div key={el.id}>  <span> <h5 className="p-3 mb-2 bg-secondary text-white"> {el.name} </h5> </span>
+                                <div>
+                                    <button type="button" className="btn btn-outline-secondary" onClick={() => props.leftButtonPushed(el.id) } > ⇦{leftButton}</button>
+                                    <button type="button" className="btn btn-outline-secondary" onClick={() => props.rightButtonPushed(el.id)} >{rightButton}⇨</button>
+                                </div>
+                            </div>)
+                    }
                 </center>
+           
             </div>
         
         );
